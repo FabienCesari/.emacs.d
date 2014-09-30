6b888a3df5bab6042844afe5e7839ea701304902
magnizdat <fabien.cesari@gmail.com>
2014-09-30 08:48:08 -0700

    python mode added to config as default

--- emacs-config.el
+++ emacs-config.el
@@ -284,4 +284,6 @@
           '(lambda ()
              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 
-;;(require 'python-mode)
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
