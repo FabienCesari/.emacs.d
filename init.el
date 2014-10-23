1d26342e428479f4bb75d8e072a80835eb72c618
magnizdat <fabien.cesari@gmail.com>
2014-10-23 13:03:22 -0700

    Fixed init.el, Carefull when using automatic variables update

--- init.el
+++ init.el
@@ -40,22 +40,3 @@
 ;;load neatly organized org file!
 (org-babel-load-file "~/.emacs.d/emacs-config.org")
 ;; End of init.el
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(gdb-max-frames 100)
- '(global-subword-mode t)
- '(ido-enable-flex-matching t)
- '(ido-everywhere t)
- '(ido-mode (quote both) nil (ido))
- '(org-agenda-files (quote ("~/work/org/ical.org" "~/work/org/agenda.org")))
- '(tool-bar-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background "black" :foreground "grey"))))
- '(fringe ((t (:background "black")))))
