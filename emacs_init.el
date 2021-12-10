21997ae996ebde9e42e867e5c1f563c8ce7a4cd5
FabienCesari <fabien.cesari@gmail.com>
2021-12-09 19:02:02 -0800

    Update emacs_init.el

--- emacs_init.el
+++ emacs_init.el
@@ -2,15 +2,15 @@
 (setq user-full-name "Fabien Cesari"
       user-mail-address "fabien.cesari@gmail.com")
 
+(add-to-list 'load-path "~/.emacs.d/external/")
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:background "black" :foreground "grey"))))
- '(fringe ((t (:background "black")))))
-
-(add-to-list 'load-path "~/.emacs.d/externals/")
+ '(fringe ((t (:background "black")))))     
 
 ;; change window focus with shift-arrows
 ;;(require 'windmove)
